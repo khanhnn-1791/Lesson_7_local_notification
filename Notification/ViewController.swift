1be617a34625d39220d70c3033e22b8a19a825b0
nkhanh44 <nguyen.nam.khanh@sun-asterisk.com>
2019-03-21 11:26:05 +0700

    local noti

--- Notification/ViewController.swift
+++ Notification/ViewController.swift
@@ -7,14 +7,46 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert]) { (granted, error) in
+            if granted {
+                print("done")
+            } else {
+                print("hell no")
+            }
+        }
     }
 
-
+    
+    @IBAction func sendAction(_ sender: Any) {
+        
+        let content = UNMutableNotificationContent()
+        content.title = "Notification"
+        content.subtitle = "team Khanh"
+        content.body = "content"
+        
+        guard let url = Bundle.main.url(forResource: "image_jess", withExtension: "jpg") else {
+            return
+        }
+        
+        let attach = try! UNNotificationAttachment(identifier: "attach", url: url   , options: .none)
+        content.attachments = [attach]
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        let request = UNNotificationRequest(identifier: "101", content: content, trigger: trigger)
+        
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+        
+        
+    }
+    
+    
 }
 
