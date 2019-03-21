bd6e1276404567b179309852116053f354ef70a1
nkhanh44 <nguyen.nam.khanh@sun-asterisk.com>
2019-03-21 10:08:42 +0700

    Initial Commit

---
+++ Notification/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notification
+//
+//  Created by nguyen.nam.khanh on 3/21/19.
+//  Copyright © 2019 nguyen.nam.khanh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
